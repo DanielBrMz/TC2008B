bd000ca15a39d1c17ce8cf1e25ef0b42db794b82
Yair Salvador <112516101+Y0z64@users.noreply.github.com>
2024-08-28 19:17:00 -0600

    Revert "Functioning simulation (#15)"
    This reverts commit 17cff2732efb7737fa12e1e434862de26b0bb433.

--- Unity/Assets/Scripts/SensorTrigger.cs
+++ Unity/Assets/Scripts/SensorTrigger.cs
@@ -4,101 +4,38 @@ public class SensorTrigger : MonoBehaviour
 {
   public Agent parentAgent;
   public char direction;
-  private TextMesh numberDisplay;
 
-  public int value = 0;
-
-  private object _lock = new object();
-
-  public void SetTextMesh(TextMesh textMesh)
-  {
-    numberDisplay = textMesh;
-    UpdateDisplay();
-  }
-
-  public void UpdataDisplayValue()
-  {
-    UpdateDisplay();
-  }
-
-  private void UpdateDisplay()
-  {
-    if (numberDisplay != null)
-    {
-      numberDisplay.text = value.ToString();
-    }
-  }
+  private int value = 0;
 
   private void OnTriggerEnter(Collider other)
   {
-    UpdateSensorValue(other);
+    value = Utils.DetermineColliderType(other.gameObject.layer);
+
+    if (parentAgent != null)
+    {
+      parentAgent.UpdateSensorValue(direction, value);
+    } else 
+    {
+      Debug.LogError("The parent agent is not set correctly");
+    }
   }
 
   private void OnTriggerStay(Collider other)
   {
-    UpdateSensorValue(other);
+    value = Utils.DetermineColliderType(other.gameObject.layer);
+
+    if (parentAgent != null)
+    {
+      parentAgent.UpdateSensorValue(direction, value);
+    } else 
+    {
+      Debug.LogError("The parent agent is not set correctly");
+    }
   }
 
   private void OnTriggerExit(Collider other)
   {
-    lock (_lock)
-    {
-      value = 0;
-      parentAgent.UpdateSensorValue(direction, 0);
-      UpdateDisplay();
-    }
-  }
-
-  private void UpdateSensorValue(Collider other)
-  {
-    lock (_lock)
-    {
-      int newValue = Utils.DetermineColliderType(other.gameObject.layer);
-      if (newValue != value)
-      {
-        value = newValue;
-        parentAgent.UpdateSensorValue(direction, value);
-        UpdateDisplay();
-      }
-    }
-  }
-
-  public void ClearSensorValue()
-  {
-    lock (_lock)
-    {
-      value = 0;
-      parentAgent.UpdateSensorValue(direction, 0);
-      UpdateDisplay();
-    }
-  }
-
-  public void ForceUpdate()
-  {
-    Collider[] colliders = Physics.OverlapSphere(transform.position, GetComponent<SphereCollider>().radius, LayerMask.GetMask("Objects", "Obstacles", "Stacks"));
-
-    if (colliders.Length > 0)
-    {
-      int newValue = Utils.DetermineColliderType(colliders[0].gameObject.layer);
-      value = newValue;
-    }
-    else
-    {
-      SetSensorValue(0);
-    }
-
-    UpdateDisplay();
-  }
-
-  private void LateUpdate()
-  {
-
-    UpdataDisplayValue();
-  }
-
-  public void SetSensorValue(int n)
-  {
-    value = n;
+    parentAgent.UpdateSensorValue(direction, 0);
   }
 
   public int GetSensorValue()
